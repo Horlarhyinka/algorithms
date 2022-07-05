270f5ba35ebef255ea0443eb389ab0419fd16512
horlarhyinka <danijufarouq2003@gmail.com>
2022-07-05 10:53:15 +0100

    sorry, one last commit,just a little DRY implementation

--- stopwatch.js
+++ stopwatch.js
@@ -1,19 +1,20 @@
 function Stopwatch (){
   let startTime, stopTime, duration = 0;
   let started = false;
+  let getTime = function(){
+    let date = new Date();
+    return date.getTime();
+  }
   this.start = function(){
     if(started)return 'stopwatch is already started';
-      let date = new Date();
-    startTime = date.getTime();
+    startTime = getTime();
     started = true;
-    return "stopwatch started"
-  
-    
+    return "stopwatch started"; 
   }
   this.stop = function(){
     if(!started) return "stopwatch is not started";
-    let date = new Date();
-    stopTime = date.getTime();
+   
+    stopTime = getTime()
     duration = stopTime - startTime;
     started = false;
     return duration/1000 + "seconds";

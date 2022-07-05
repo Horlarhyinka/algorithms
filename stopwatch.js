3db4063ff394f43dbb8bf97d0ce40380006a31a2
horlarhyinka <danijufarouq2003@gmail.com>
2022-07-05 10:48:31 +0100

    corrections made and algorithm completed moving to next algorithm 'car-park'

--- stopwatch.js
+++ stopwatch.js
@@ -1,15 +1,22 @@
-funcion Stopwatch (){
+function Stopwatch (){
   let startTime, stopTime, duration = 0;
   let started = false;
   this.start = function(){
-    if(this.started) return 'stopwatch is already started';
-    let date = new Date();
+    if(started)return 'stopwatch is already started';
+      let date = new Date();
     startTime = date.getTime();
     started = true;
     return "stopwatch started"
+  
+    
   }
   this.stop = function(){
-    if(!this.started) return 'stopwatch is not started';
+    if(!started) return "stopwatch is not started";
+    let date = new Date();
+    stopTime = date.getTime();
+    duration = stopTime - startTime;
+    started = false;
+    return duration/1000 + "seconds";
     
   }
 }
